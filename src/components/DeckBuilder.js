ef6a8a553ffd1abc0662f8cb7f4b089d00154785
dgiraldo313 <dgiraldo313@drew.edu>
2016-11-03 22:28:02 -0400

    Created module to build the deck of cards
    Module builds decks of cards and assigns values and exercises
    dynamically to each of the cards

--- src/components/DeckBuilder.js
+++ src/components/DeckBuilder.js
@@ -2,21 +2,37 @@ import React from 'react';
 
 let Deck = [];
 
-const CreateCardDeck = () => {
+const CreateCardDeck = (ExerciseList) => {
+  console.log('List of Exercises: ', ExerciseList);
   let Suits = ['DIAMONDS', 'SPADES', 'CLUBS', 'HEARTS'];
+  let Ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
   // create deck
-  for (var i = 2; i < 15; i++) {
-    for (var j = 0; j < Suits.length; j++) {
+  Suits.map((suit) => {
+    Ranks.map((rank, value) => {
+      // temp store suit of current card
+      let Suit = suit;
+
+      // get exercise depending on suit
+      let Exercise = ExerciseList[Suit].exercise;
+
+      // store rank of card
+      let Rank = rank;
+
+      // get value of card, if suit is a letter than value is 10, otherwise is value of card
+      let Value = rank == 'A' || rank == 'J' || rank == 'Q' || rank == 'K' ? 10 : value + 1;
       let Card = {
-                  suite: Suits[j],
-                  value: i,
+                  suit: Suit,
+                  rank: Rank,
+                  exercise: Exercise,
+                  reps: Value,
+
                 };
-      
+
       // add card object to deck
       Deck.push(Card);
-    }
-  }
+    });
+  });
 };
 
 const ShuffleCardDeck = () => {
@@ -28,7 +44,7 @@ const ShuffleCardDeck = () => {
   let tempValue;
   let randomIndex;
 
-  while (!currentIndex) {
+  while (currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
     tempValue = cardArray[currentIndex];
@@ -39,4 +55,9 @@ const ShuffleCardDeck = () => {
   return cardArray;
 };
 
-export { CreateCardDeck, ShuffleCardDeck };
+const getDeckOfCards = (ExerciseList) => {
+  let CardDeck = CreateCardDeck(ExerciseList);
+  return ShuffleCardDeck(CardDeck);
+};
+
+export { getDeckOfCards };
